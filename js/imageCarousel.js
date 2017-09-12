baa9d31736e72477d90004964148dfbe11999572
YesidDaza <u1801475@unimilitar.edu.co>
2017-09-12 14:30:13 -0500

    imageCarousel is uploaded

---
+++ js/imageCarousel.js
@@ -0,0 +1,53 @@
+<script>
+    window.onload = function() {
+      var galleryClass = ".images-carousel";
+      var imageContainerClass = " .jcarousel-item .views-field-field-imagenes-de-la-jornada .field-content";
+      var galleryItemContainers = document.querySelectorAll(galleryClass + " .jcarousel-item");
+      var galleryImages = document.querySelectorAll(galleryClass + " img");
+      var galleryImagesContainers = document.querySelectorAll(galleryClass + imageContainerClass);
+
+      buildModalButtons (galleryImages, galleryImagesContainers, galleryClass + imageContainerClass);
+      buildModalViews(galleryImages, galleryItemContainers, galleryClass);
+    };
+
+    function buildModalButtons (imagesArray, imagesContainersArray, containerClass) {
+      console.log(imagesArray);
+      console.log(imagesContainersArray);
+      if ((imagesArray.length == imagesContainersArray.length) && (imagesArray.length > 0)) {
+        for (var i = 0; i < imagesArray.length; i++) {
+
+          var modalButtonStartTag = "<button type='button' class='btn btn-info btn-lg' data-toggle='modal' data-target='#imageModal";
+          var modalButtonFinishTag = "'></button>";
+          var modalButton = modalButtonStartTag + i + modalButtonFinishTag;
+          imagesContainersArray[i].innerHTML = modalButton;
+          var galleryImagesButtons = document.querySelectorAll(containerClass + " button");
+          galleryImagesButtons[i].appendChild(imagesArray[i]);
+        }
+      }
+    }
+
+    function buildModalViews(imagesArray, itemContainersArray, galleryClass) {
+      if ((imagesArray.length == itemContainersArray.length) && (imagesArray.length > 0)) {
+        for (var i = 0; i < imagesArray.length; i++) {
+          var modalView = getModalView("Imagen " + (i+1) + " de " + imagesArray.length, i);
+          var modalViewContainer = document.createElement('div');
+          modalViewContainer.innerHTML = modalView;
+          itemContainersArray[i].appendChild(modalViewContainer);
+          var modalBodyArray = document.querySelectorAll(galleryClass + " .modal-body");
+          var modalImage = imagesArray[i].cloneNode(true);
+          modalBodyArray[i].appendChild(modalImage);
+        }
+      }
+    }
+
+    function getModalView(modalTitle, index) {
+      var modalViewStarTag = "<div id='imageModal"+ index + "' class='modal fade' role='dialog'><div class='modal-dialog'><div class='modal-content'>";
+      var modalHeaderStarTag = "<div class='modal-header'><button type='button' class='close' data-dismiss='modal'>&times;</button><h4 class='modal-title'>";
+      var modalHeaderFinishTag = "</h4></div>";
+      var modalHeaderTag = modalHeaderStarTag + modalTitle + modalHeaderFinishTag;
+      var modalBodyTag = "<div class='modal-body'></div>";
+      var modalViewFinishTag = "</div></div></div>";
+      var modalView = modalViewStarTag + modalHeaderTag + modalBodyTag + modalViewFinishTag;
+      return modalView;
+    }
+</script>
